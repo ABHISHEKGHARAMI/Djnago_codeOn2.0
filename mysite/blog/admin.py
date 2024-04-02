117e2842867f7fefff8e6382c99a170293e82d76
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-04-03 01:23:15 +0530

    Adding the model to the admin for better use of the application .

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+# have to register the model to the admin for the user freindly interface to the admin site
+from .models import Post
 # Register your models here.
+
+admin.site.register(Post)
