526c692f61f09fb48fcea04fa1e41c5da0e810de
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-04-02 11:38:13 +0530

    Adding the models of the blog application .

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+# first we will create the Post model
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.CharField(max_length=250)
+    body = models.TextField()
+    
+    # method for title
+    def __str__(self):
+        return self.title
