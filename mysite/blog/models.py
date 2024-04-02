f90bd38d167008593a0fadd88ef83fc5dbccc601
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-04-02 11:52:40 +0530

    Adding time as the parameter to the model and reverse ordering the posts using the meta data in the model .

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+# importing time zone
+from django.utils import timezone
 # Create your models here.
 
 
@@ -8,6 +9,18 @@ class Post(models.Model):
     title = models.CharField(max_length=250)
     slug = models.CharField(max_length=250)
     body = models.TextField()
+    # adding the time field as the parameter of the model Post.
+    publish = models.TimeField(default=timezone.now)
+    created = models.TimeField(auto_now_add=True)
+    updated = models.TimeField(auto_now=True)
+    
+    
+    # basically what happens is the posts to be shown in the website should be reverse order
+    # like newer post are first then the older post
+    # thats why we are creating the meta data of the model 
+    
+    class Meta:
+        ordering = ['-publish']
     
     # method for title
     def __str__(self):
