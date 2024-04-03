0544ff10150c5793fa1a255d11874642640787f8
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-04-03 10:23:38 +0530

    Adding the 2nd view post detail for every post based on their id .

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from django.http import Http404
 # Create your views here.
 
 # first view
@@ -10,5 +11,20 @@ def post_list(request):
         'blog/post/list.html',
         {
             'posts':posts,
+        }
+    )
+
+# 2nd view post detail
+def post_detail(request,id):
+    try:
+        post = Post.published.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404('Post does not exist')
+    
+    return render(
+        request,
+        'blog/post/details.html',
+        {
+            'post' : post
         }
     )
