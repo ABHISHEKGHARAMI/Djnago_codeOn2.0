618054b19b611bac43bd6260183aa57417b82523
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-04-03 10:17:21 +0530

    Added the first view post_list to the views.py .

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+
+# first view
+def post_list(request):
+    posts = Post.published.all()
+    return render(
+        request,
+        'blog/post/list.html',
+        {
+            'posts':posts,
+        }
+    )
