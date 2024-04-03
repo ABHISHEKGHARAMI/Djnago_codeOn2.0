89593cfedfe4e70bbbe48c6da276522718852d31
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-04-03 10:28:49 +0530

    Adding the get_objects_or_404 method to the post_details view.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from .models import Post
 from django.http import Http404
 # Create your views here.
@@ -16,10 +16,17 @@ def post_list(request):
 
 # 2nd view post detail
 def post_detail(request,id):
-    try:
+    '''try:
         post = Post.published.get(id=id)
     except Post.DoesNotExist:
-        raise Http404('Post does not exist')
+        raise Http404('Post does not exist')'''
+    #can do same thing with the help of the get_object_or_404
+    
+    post = get_object_or_404(
+        Post,
+        id = id,
+        status = Post.Status.PUBLISHED
+    )
     
     return render(
         request,
