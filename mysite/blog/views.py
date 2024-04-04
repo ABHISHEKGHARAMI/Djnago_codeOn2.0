ba2db7783e5f462d855b6034129a13e899027439
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-04-04 18:59:16 +0530

    Successfully added paginator .

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render , get_object_or_404
 from .models import Post
 from django.http import Http404
+#adding the paginator
+from django.core.paginator import Paginator
 # Create your views here.
 
 # first view
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
     #print(posts)
+    paginator = Paginator(post_list,3)
+    page_number = request.GET.get('page',1)
+    posts = paginator.page(page_number)
     return render(
         request,
         'blog/post/list.html',
