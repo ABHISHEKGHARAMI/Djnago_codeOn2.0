b1da4eb06a6d1d77702cf5f3163b24e889a771b7
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-04-08 15:29:01 +0530

    Adding the modelform to the build the from dynamically using the django .

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -1,6 +1,9 @@
 # creating the form for sharing user post
 from django import forms
 
+# import comments from the model
+from .models import Comment
+
 class EmailPostForm(forms.Form):
     # adding the different field
     name = forms.CharField(max_length=25)
@@ -9,3 +12,10 @@ class EmailPostForm(forms.Form):
     comments = forms.CharField(
         required=False,
         widget=forms.Textarea)
+    
+    
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['name','email','body']
+        
