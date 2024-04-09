b4fe8af8c395a83b0424d0669620e9803c58a5da
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-04-09 19:03:40 +0530

    Successfully added the latest posts in template in inclusion_tags custom tags .

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -7,3 +7,11 @@ register = template.Library()
 @register.simple_tag
 def total_posts():
     return Post.published.count()
+
+# inclusion tags
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {
+        'latest_posts' : latest_posts
+    }
