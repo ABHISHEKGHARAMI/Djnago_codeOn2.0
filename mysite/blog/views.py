edae0f33035ae72fbb4f540b9491900964425b2f
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-04-09 18:32:59 +0530

    Added the similar posts in the template.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -71,12 +71,10 @@ def post_detail(request,year,month,day,post):
     form = CommentForm()
     
     # adding the similar post excluding the post
-    post_tag_ids = post.tags.values_list('id',
-                                         flat=True)
-    similar_posts = Post.published.filter(tags__in=post_tag_ids).exclude(id=post.id)
-    similar_posts = similar_posts.annotate(same_tags = Count('tags')).order_by(['-same_tags',
-                                                                                '-publish'])[:4]
-    
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4] 
+    #print(similar_posts)
     
     return render(
         request,
