90a328b556d2af4946cf35796f21fc44a1800f21
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-04-14 15:49:53 +0530

    Successfully added the postgresql database to the repo .

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -80,10 +80,18 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+     #   'ENGINE': 'django.db.backends.sqlite3',
+     #   'NAME': BASE_DIR / 'db.sqlite3',
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'blog3',
+    'USER': 'blog3',
+    'PASSWORD': 'Abhi1998@',
+    'HOST' : 'localhost'
+}
+   
 }
 
 
