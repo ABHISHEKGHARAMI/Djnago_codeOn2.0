c3cd6d5e7c522a7d9b5c0cc4e81814f4fd5d0cbd
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-04-14 16:07:14 +0530

    mod .

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -18,4 +18,10 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['name','email','body']
-        
+
+
+
+# building the  queryform
+class SearchForm(forms.Form):
+    query = forms.CharField()
+      
