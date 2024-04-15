eeb80457c656fb4ec01b181f545dddc04b157fb0
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-04-15 09:43:01 +0530

    Adding the stemming in the search .

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -15,7 +15,7 @@ from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 
 # adding the search vector for the search
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector , SearchQuery , SearchRank
 
 # importing the django.taggit
 from taggit.models import Tag
@@ -156,8 +156,11 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title','body')
+            search_query = SearchQuery(query)
             results = Post.published.annotate(
-             search=SearchVector('title', 'body'),).filter(search=query)
+             search=search_vector,
+             rank = SearchRank(search_vector,search_query)).filter(search=search_query).order_by('-rank')
     return render(request,
               'blog/post/search.html',
               {'form': form,
